d744c5006725eddb761be9f58268669c19d161b9
Dennis Demir <dennisjdemir@gmail.com>
2023-06-06 23:36:14 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,30 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType(e){
+    setFilters({type: e.target.value})
+  }
+
+  function findPetsClick(){
+    if (filters.type !== 'all'){
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then((r) => r.json())
+      .then((pets) => setPets(pets))
+    } else {
+      fetch('http://localhost:3001/pets')
+      .then((r) => r.json())
+      .then((pets) => setPets(pets))
+    }
+  }
+
+  function handleAdoptPetClick(id){
+    const updatedPets = pets.map((pet) => {
+      if (id === pet.id){
+        return {...pet, isAdopted: true}
+      } else {return pet}
+    })
+    setPets(updatedPets)
+  }
   return (
     <div className="ui container">
       <header>
@@ -15,10 +39,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={findPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPetClick}/>
           </div>
         </div>
       </div>
